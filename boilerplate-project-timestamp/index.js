efac646098416036c66c327e9a90957f3d82c3ba
Luis Padron <luispadron25@gmail.com>
2024-09-24 12:49:52 -0500

    Timestamp logic added

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -1,32 +1,48 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
+var cors = require("cors");
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api", (req, res) => {
+  const currentDate = new Date();
+  const unix = currentDate.getTime();
+  const date = currentDate.toUTCString();
+  return res.json({ unix: unix, utc: date });
+});
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+const dateHandler = (req, res) => {
+  const date = req.params.date;
 
+  if (!isNaN(date)) {
+    const timestamp =
+      date.length === 13 ? parseInt(date) : parseInt(date) * 1000;
+    const utc = new Date(timestamp).toUTCString();
+    return res.json({ unix: timestamp, utc: utc });
+  }
 
-// Listen on port set in environment variable or default to 3000
+  const parsedDate = new Date(date);
+
+  if (!isNaN(parsedDate.getTime())) {
+    const unix = parsedDate.getTime();
+    const utc = parsedDate.toUTCString();
+    return res.json({ unix: unix, utc: utc });
+  } else {
+    return res.json({ error: "Invalid Date" });
+  }
+};
+
+app.get("/api/:date", dateHandler);
+
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
